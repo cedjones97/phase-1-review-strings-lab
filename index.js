499306e53e00a554f5363a7178c64c0dccf60e77
cedric <cedricj765@gmail.com>
2023-03-05 09:12:07 -0600

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = "Grace Hooper"
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+let excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toLocaleUpperCase()}!`
+let shortGreeting = `Welcome, ${currentUser.charAt(0)}!`
